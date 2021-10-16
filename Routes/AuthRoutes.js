590598c53f116a4696998cc4bf63fde416f11c04
arpitgoyal442 <arpitgoyal442@gmail.com>
2021-10-16 12:36:28 +0530

    fourth

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -6,21 +6,21 @@ const router = express.Router();
 
 router.post('/login', (req, res)=>
 {
-    const first=req.body.it.HU;
-    const last=req.body.it.YS;
-    const email=req.body.it.Tt;
-    const photo=req.body.it.kK ;
-    // const ip= req.socket.remoteAddress;
-    const ip="0:0:1";
+    // const first=req.body.it.HU;
+    // const last=req.body.it.YS;
+    // const email=req.body.it.Tt;
+    // const photo=req.body.it.kK ;
+    // // const ip= req.socket.remoteAddress;
+    // const ip="0:0:1";
 
 
     // To check in postman use this below code and comment above
 
-    // const first=req.body.firstName;
-    // const last=req.body.lastName;
-    // const email=req.body.email;
-    // const photo=req.body.photo;
-    // const ip= req.body.ip;
+    const first=req.body.firstName;
+    const last=req.body.lastName;
+    const email=req.body.email;
+    const photo=req.body.photo;
+    const ip= req.body.ip;
 
     
 
