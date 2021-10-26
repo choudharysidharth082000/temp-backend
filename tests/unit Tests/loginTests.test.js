438a406ec946d48b0982d0e7628f84fc27165e49
arpitgoyal442 <arpitgoyal442@gmail.com>
2021-10-26 10:24:56 +0530

    Auth Route

--- tests/unit Tests/loginTests.test.js
+++ tests/unit Tests/loginTests.test.js
@@ -18,9 +18,9 @@ describe('GET /v1/auth/sampleTest',()=>
     it('Getting Rest Api', ()=>
     {
         request(app)
-  .get('/v1/auth/testSample')
-  .expect(200)
-  .end(function(err, res) {
+       .get('/v1/auth/testSample')
+       .expect(200)
+     .end(function(err, res) {
     if (err) throw err;
   });
     })
